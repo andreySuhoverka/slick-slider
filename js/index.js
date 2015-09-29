91920499d6345b5e31f0724a99a12f2d786cab12
Andrei Sukhoverka <Andrei_Sukhoverka@epam.com>
2015-09-29 17:10:29 +0300

    last commit

--- js/index.js
+++ js/index.js
@@ -1,12 +1,9 @@
 $(document).ready(function() {
 
     $('.responsive').slick({
-        // dots: true,
-        infinite: true,
-        speed: 300,
-        slidesToShow: 8,
+        slidesToShow: 6,
         slidesToScroll: 1,
-        variableWidth: true,
+
         responsive: [{
             breakpoint: 1024,
             settings: {
@@ -21,8 +18,6 @@ $(document).ready(function() {
             settings: {
                 slidesToShow: 3,
                 slidesToScroll: 2,
-                dots: true,
-                infinite: true,
 
             }
 
@@ -32,19 +27,13 @@ $(document).ready(function() {
             settings: {
                 slidesToShow: 2,
                 slidesToScroll: 2,
-                dots: true,
-                infinite: true,
-                
+
             }
         }, {
             breakpoint: 480,
             settings: {
                 slidesToShow: 1,
                 slidesToScroll: 1,
-                dots: true,
-                infinite: true,
-                autoplay: true,
-                autoplaySpeed: 2000,
             }
         }]
     });
